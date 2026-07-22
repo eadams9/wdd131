520d4e7f20054a3a9a35d7d05d922c7fe5dd128e
eadams9 <adamsadamaba@gmail.com>
2026-07-22 11:44:58 +0000

    Add HTML structure, CSS styles, and JavaScript for Ghana profile page with weather data

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,12 +2,25 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+button.addEventListener('click', function () {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
-deleteButton.classList.add('delete');
-li.appendChild(deleteButton);
-list.append(li);
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+    deleteButton.classList.add('delete');
+    li.appendChild(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
+  }
+
+  input.focus();
+});
